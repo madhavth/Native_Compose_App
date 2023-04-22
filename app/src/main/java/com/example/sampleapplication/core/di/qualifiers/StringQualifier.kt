90549f9469c053096ade6feb555806668ed5e6bf
madhavth <madhavth77@gmail.com>
2023-04-22 13:23:37 -0600

    added required libraries

---
+++ app/src/main/java/com/example/sampleapplication/core/di/qualifiers/StringQualifier.kt
@@ -0,0 +1,12 @@
+package com.example.sampleapplication.core.di.qualifiers
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class HelloQualifier
+
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class WorldQualifier
